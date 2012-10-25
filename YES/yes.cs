61de8a89f17403308e68ad6b1aa2166ce95523be
Andr'U Sender <andru.sender@gmail.com>
2012-10-26 01:04:31 +0300

    First

---
+++ YES/yes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace YES
+{
+    public partial class yes : Form
+    {
+        public yes()
+        {
+            InitializeComponent();
+        }
+    }
+}
